367dad4b861da4d4b977c9301e1ed8d4cfc82096
ludovickNinja <44409534+ludovickNinja@users.noreply.github.com>
2021-09-08 13:48:17 -0400

    forgot a container

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -112,7 +112,7 @@ function init() {
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
 	renderer.toneMappingExposure = 1;
 	renderer.outputEncoding = THREE.sRGBEncoding;
-	container.appendChild( renderer.domElement );
+	//container.appendChild( renderer.domElement );
 
     
 	const controls = new OrbitControls( camera, renderer.domElement );
