7584d721155a65948e9892086057dc9b34267a15
ludovickNinja <44409534+ludovickNinja@users.noreply.github.com>
2021-09-08 13:50:46 -0400

    ./three

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from './three';
 
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
