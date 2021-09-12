7ef81865bdc0c9821449533bbfd0b85819079419
Ludovick <design@ludovick.ninja>
2021-09-12 18:36:39 -0400

    Added firstThree

---
+++ firstThree/main.js
@@ -0,0 +1,238 @@
+import * as THREE from 'https://cdn.skypack.dev/three@latest';
+
+import { OrbitControls } from 'https://cdn.skypack.dev/three@latest/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@latest/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'https://cdn.skypack.dev/three@latest/examples/jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from 'https://cdn.skypack.dev/three@latest/examples/jsm/utils/RoughnessMipmapper.js';
+
+let camera, scene, renderer, canvasWidth, canvasHeight, object, controls;
+
+let SCREEN_WIDTH = window.innerWidth;
+let SCREEN_HEIGHT = window.innerHeight;
+let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+const clock = new THREE.Clock()
+
+init();
+
+console.log(camera.position.z);
+console.log(camera.position.z);
+
+render();
+
+function init() {
+
+  //canvasHeight = window.innerHeight - document.getElementById('myHeader').children[0].clientHeight;
+  canvasHeight = window.innerHeight - document.getElementById('myHeader').clientHeight;
+  //canvasWidth = document.body.clientWidth - (25*2);
+  canvasWidth = document.getElementById('myHeader').children[0].clientWidth;
+  
+  /*
+	const container = document.createElement( 'div' );
+	document.body.appendChild( container );
+  */
+  
+  const canvas = document.querySelector('canvas.webgl')
+
+	camera = new THREE.PerspectiveCamera( 1.25, canvasWidth / canvasHeight * 100 / 100, 1, 2000 );
+	camera.position.set( -0.5, 0.75, 1 );
+  camera.lookAt( 0, 0, 0 );
+  camera.setFocalLength(canvasHeight + canvasWidth);
+  camera.updateProjectionMatrix;
+
+	scene = new THREE.Scene();
+
+    //Loaders
+	new RGBELoader()
+	//.setPath( 'textures/equirectangular/' )
+	.load( 
+        // resource URL
+        'https://ludovickninja.github.io/Studio.hdr', 
+        // called when the resource is loaded
+        function ( texture ) {
+		texture.mapping = THREE.EquirectangularReflectionMapping;
+
+		//scene.background = texture;
+        scene.background = new THREE.Color( 0xffffff );
+		scene.environment = texture;
+
+		render();
+
+		// use of RoughnessMipmapper is optional
+		const roughnessMipmapper = new RoughnessMipmapper( renderer );
+
+		const loader = new GLTFLoader()
+        //.setPath( 'models/gltf/DamagedHelmet/glTF/' );
+		loader.load( 
+        // model URL
+        'https://ludovickninja.github.io/MTL1.glb', 
+        // called when the model is loaded
+        function ( gltf ) {
+
+            gltf.scene.traverse( function ( child ) {
+
+				if ( child.isMesh ) {
+
+					roughnessMipmapper.generateMipmaps( child.material );
+
+				}
+
+			} );
+
+            object = gltf.scene;
+            object.position.set(0, 0, 0);
+
+			scene.add( gltf.scene );
+
+            const tick = () =>
+            {
+            
+                const elapsedTime = clock.getElapsedTime()
+            
+                // Update objects
+                object.rotation.y = .5 * elapsedTime
+
+                // Update Orbital Controls
+                controls.update()
+            
+                // Render
+                render();
+            
+                // Call tick again on the next frame
+                window.requestAnimationFrame(tick)
+            }
+                        
+			roughnessMipmapper.dispose();
+
+            tick()
+		},
+        
+        function ( xhr ) {
+            // called while loading is progressing
+            console.log( `${( xhr.loaded / xhr.total * 100 )}% loaded` );
+        },
+
+        function ( error ) {
+            // called when loading has errors
+            console.error( 'An error happened', error );
+        },
+        );
+
+	} 
+    );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true, canvas: canvas } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( canvasWidth, canvasHeight );
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.toneMappingExposure = 1;
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	//container.appendChild( renderer.domElement );
+    
+  controls = new OrbitControls( camera, renderer.domElement );
+	controls.addEventListener( 'change', render ); // use if there is no animation loop
+  controls.minDistance = 2;
+	//controls.maxDistance = controls.minDistance;
+  controls.maxDistance = 10;  
+	controls.target.set( 0, 0, 0);
+  controls.enableDamping = true
+  controls.update();
+    
+	window.addEventListener( 'resize', onWindowResize );
+  
+}
+
+function onWindowResize() {
+
+  canvasHeight = window.innerHeight - document.getElementById('myHeader').clientHeight;
+  canvasWidth = document.body.clientWidth - (25*2);
+
+  const ratio = calculateAspectRatioFit(10000, 10000, canvasWidth, canvasHeight)
+
+	camera.aspect = canvasWidth / canvasHeight;
+  camera.setFocalLength(canvasHeight + canvasWidth);
+  
+	//camera.aspect = canvasWidth / canvasHeight * 1.25;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( canvasWidth  , canvasHeight );
+	//renderer.setSize( ratio.width , ratio.height );
+
+  console.log(camera.aspect)
+
+	render();
+
+}
+
+function render() {
+
+	renderer.render( scene, camera );
+
+}
+
+function fitCameraToObject ( camera, object, offset, controls ) {
+
+	offset = offset || 5;
+
+	const boundingBox = new THREE.Box3();
+
+	// get bounding box of object - this will be used to setup controls and camera
+	boundingBox.setFromObject( object );
+
+	const center = boundingBox.getCenter();
+
+	const size = boundingBox.getSize();
+
+	// get the max side of the bounding box (fits to width OR height as needed )
+	const maxDim = Math.max( size.x, size.y, size.z );
+	const fov = camera.fov * ( Math.PI / 180 );
+	let cameraZ = Math.abs( maxDim / 2 * Math.tan( fov * 2 ) ); //Applied fifonik correction
+
+	cameraZ *= offset; // zoom out a little so that objects don't fill the screen
+
+	// <--- NEW CODE
+	//Method 1 to get object's world position
+	scene.updateMatrixWorld(); //Update world positions
+	var objectWorldPosition = new THREE.Vector3();
+	objectWorldPosition.setFromMatrixPosition( object.matrixWorld );
+	
+	//Method 2 to get object's world position
+	//objectWorldPosition = object.getWorldPosition();
+
+	const directionVector = camera.position.sub(objectWorldPosition); 	//Get vector from camera to object
+	const unitDirectionVector = directionVector.normalize(); // Convert to unit vector
+	camera.position = unitDirectionVector.multiplyScalar(cameraZ); //Multiply unit vector times cameraZ distance
+	camera.lookAt(objectWorldPosition); //Look at object
+	// --->
+
+	const minZ = boundingBox.min.z;
+	const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+
+	camera.far = cameraToFarEdge * 4;
+	camera.updateProjectionMatrix();
+
+	if ( controls ) {
+
+	  // set camera to rotate around center of loaded object
+	  controls.target = center;
+
+	  // prevent camera from zooming out far enough to create far plane cutoff
+	  controls.maxDistance = cameraToFarEdge * 2;
+
+	  controls.saveState();
+
+	} else {
+
+		camera.lookAt( center )
+
+   }
+}
+
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+  var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+  return { width: srcWidth*ratio, height: srcHeight*ratio, ratioValue: ratio };
+
+  //return Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+}
