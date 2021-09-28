3b2f6ad1fb4061a1e5236ba06dd98285b4779d98
Ludovick <design@ludovick.ninja>
2021-09-28 19:32:00 -0400

    Update to thirdThree

--- thirdThree/main.js
+++ thirdThree/main.js
@@ -21,16 +21,9 @@ const canvas = document.querySelector('canvas.webgl')
 canvasHeight = canvasContainer.offsetHeight;
 canvasWidth = canvasContainer.offsetWidth;
 
-select.selectedIndex = 0;
+init(file);
+render();
 
-select.addEventListener('change', () => {
-	modelHeader.innerText = select.value;
-	file = 'https://ludovickninja.github.io/' + select.value + '.glb'
-	console.log(file)
-
-	init(file);
-	render();
-});
 /*
 navigator.mediaDevices.getUserMedia({video: true})
   .then(gotMedia)
@@ -91,15 +84,12 @@ function init(file) {
             object.position.set(0, 0, 0);
 
 			console.log(object);
-			//console.log(getObjCenter(object));
-			//console.log(object.getCenter());
 
 			scene.add( object );
 
             const tick = () =>
             {
-            
-                const elapsedTime = clock.getElapsedTime()
+            	const elapsedTime = clock.getElapsedTime()
             
                 // Update objects
                 object.rotation.y = .5 * elapsedTime
@@ -139,8 +129,6 @@ function init(file) {
 	camera = new THREE.PerspectiveCamera( 1.25, canvasWidth / canvasHeight, 1, 2000 );
 	camera.position.set( -0.5, 0.75, 1 );
 	camera.lookAt(0, 0, 0);
-	//console.log(object);
-	//console.log(object.center);
   	//camera.lookAt(object.getWorldPosition());
   	//camera.setFocalLength(canvasHeight + canvasWidth);
   	camera.updateProjectionMatrix;
@@ -148,13 +136,13 @@ function init(file) {
 	///
 	/// Set Renderer
 	///
-	renderer = new THREE.WebGLRenderer( { antialias: true, canvas: canvas } );
+	renderer = new THREE.WebGLRenderer( { antialias: true, canvas: canvas, alpha: true } );
+	renderer.setClearColor( 0x000000, 0 );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( canvasWidth, canvasHeight );
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
 	renderer.toneMappingExposure = 1;
 	renderer.outputEncoding = THREE.sRGBEncoding;
-	//container.appendChild( renderer.domElement );
 
 	///
 	/// Set Controls
@@ -162,7 +150,6 @@ function init(file) {
   	controls = new OrbitControls( camera, renderer.domElement );
 	controls.addEventListener( 'change', render ); // use if there is no animation loop
   	controls.minDistance = 2;
-	//controls.maxDistance = controls.minDistance;
   	controls.maxDistance = 10;  
 	controls.target.set( 0, 0, 0);
   	controls.enableDamping = true
@@ -193,84 +180,5 @@ function onWindowResize() {
 function render() {
 
 	renderer.render( scene, camera );
-
-}
-
-function getObjCenter ( object ) {
-
-	const boundingBox = new THREE.Box3();
-
-	// get bounding box of object - this will be used to setup controls and camera
-	boundingBox.setFromObject( object );
-
-	const center = boundingBox.getCenter();
-
-	return center;
-
-} 
-
-function fitCameraToObject ( camera, object, offset, controls ) {
-
-	offset = offset || 5;
-
-	const boundingBox = new THREE.Box3();
-
-	// get bounding box of object - this will be used to setup controls and camera
-	boundingBox.setFromObject( object );
-
-	const center = boundingBox.getCenter();
-
-	const size = boundingBox.getSize();
-
-	// get the max side of the bounding box (fits to width OR height as needed )
-	const maxDim = Math.max( size.x, size.y, size.z );
-	const fov = camera.fov * ( Math.PI / 180 );
-	let cameraZ = Math.abs( maxDim / 2 * Math.tan( fov * 2 ) ); //Applied fifonik correction
-
-	cameraZ *= offset; // zoom out a little so that objects don't fill the screen
-
-	// <--- NEW CODE
-	//Method 1 to get object's world position
-	scene.updateMatrixWorld(); //Update world positions
-	var objectWorldPosition = new THREE.Vector3();
-	objectWorldPosition.setFromMatrixPosition( object.matrixWorld );
-	
-	//Method 2 to get object's world position
-	//objectWorldPosition = object.getWorldPosition();
-
-	const directionVector = camera.position.sub(objectWorldPosition); 	//Get vector from camera to object
-	const unitDirectionVector = directionVector.normalize(); // Convert to unit vector
-	camera.position = unitDirectionVector.multiplyScalar(cameraZ); //Multiply unit vector times cameraZ distance
-	camera.lookAt(objectWorldPosition); //Look at object
-	// --->
-
-	const minZ = boundingBox.min.z;
-	const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
-
-	camera.far = cameraToFarEdge * 4;
-	camera.updateProjectionMatrix();
-
-	if ( controls ) {
-
-	  // set camera to rotate around center of loaded object
-	  controls.target = center;
-
-	  // prevent camera from zooming out far enough to create far plane cutoff
-	  controls.maxDistance = cameraToFarEdge * 2;
-
-	  controls.saveState();
-
-	} else {
-
-		camera.lookAt( center )
-
-   }
-}
-
-function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
-
-  	var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
-
-  	return { width: srcWidth*ratio, height: srcHeight*ratio, ratioValue: ratio };
 
 }
