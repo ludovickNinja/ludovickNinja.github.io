021fa67076ecd1ada5b2499b895b833ca471cd08
Ludovick <design@ludovick.ninja>
2021-09-30 20:08:17 -0400

    Set ThreeeJS version

--- Model_Viewer/main.js
+++ Model_Viewer/main.js
@@ -1,9 +1,9 @@
-import * as THREE from 'https://cdn.skypack.dev/three@latest';
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.0';
 
-import { OrbitControls } from 'https://cdn.skypack.dev/three@latest/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@latest/examples/jsm/loaders/GLTFLoader.js';
-import { RGBELoader } from 'https://cdn.skypack.dev/three@latest/examples/jsm/loaders/RGBELoader.js';
-import { RoughnessMipmapper } from 'https://cdn.skypack.dev/three@latest/examples/jsm/utils/RoughnessMipmapper.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.0/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'https://cdn.skypack.dev/three@0.132.0/examples/jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from 'https://cdn.skypack.dev/three@0.132.0/examples/jsm/utils/RoughnessMipmapper.js';
 
 let modelHeader = document.querySelector('h2');
 let select = document.querySelector('select');
