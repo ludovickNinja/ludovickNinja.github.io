8103bbe3d3519b1cafd52368421b204a574dfa19
Ludovick <design@ludovick.ninja>
2021-10-17 00:05:31 -0400

    adjusted capture button query selector

--- myLibTest/main.js
+++ myLibTest/main.js
@@ -27,7 +27,7 @@ frontViewButton.addEventListener("click", SetFrontView);
 perspectiveViewButton.addEventListener("click", SetPerspectiveView);
 turntableControlButton.addEventListener("click", ToggleTurnTable);
 
-const captureControlButton = document.querySelector('CaptureButton');
+const captureControlButton = document.querySelector('.CaptureButton');
 captureControlButton.addEventListener("click", saveAsImage);
 
 canvasHeight = canvasContainer.offsetHeight;
