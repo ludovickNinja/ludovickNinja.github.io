fc8c7fa620a0811931c41ebd5f2e12b10dcfc9db
Ludovick <design@ludovick.ninja>
2021-10-24 14:54:21 -0400

    cleaning

--- myLibTest/main.js
+++ myLibTest/main.js
@@ -303,64 +303,6 @@ function ToggleTurnTable(btn) {
 	}
 } 
 
-function fitCameraToObject ( camera, object, offset, controls ) {
-
-	offset = offset || 5;
-
-	const boundingBox = new THREE.Box3();
-
-	// get bounding box of object - this will be used to setup controls and camera
-	boundingBox.setFromObject( object );
-
-	const center = boundingBox.getCenter();
-
-	const size = boundingBox.getSize();
-
-	// get the max side of the bounding box (fits to width OR height as needed )
-	const maxDim = Math.max( size.x, size.y, size.z );
-	const fov = camera.fov * ( Math.PI / 180 );
-	let cameraZ = Math.abs( maxDim / 2 * Math.tan( fov * 2 ) ); //Applied fifonik correction
-
-	cameraZ *= offset; // zoom out a little so that objects don't fill the screen
-
-	// <--- NEW CODE
-	//Method 1 to get object's world position
-	scene.updateMatrixWorld(); //Update world positions
-	var objectWorldPosition = new THREE.Vector3();
-	objectWorldPosition.setFromMatrixPosition( object.matrixWorld );
-	
-	//Method 2 to get object's world position
-	//objectWorldPosition = object.getWorldPosition();
-
-	const directionVector = camera.position.sub(objectWorldPosition); 	//Get vector from camera to object
-	const unitDirectionVector = directionVector.normalize(); // Convert to unit vector
-	camera.position = unitDirectionVector.multiplyScalar(cameraZ); //Multiply unit vector times cameraZ distance
-	camera.lookAt(objectWorldPosition); //Look at object
-	// --->
-
-	const minZ = boundingBox.min.z;
-	const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
-
-	camera.far = cameraToFarEdge * 4;
-	camera.updateProjectionMatrix();
-
-	if ( controls ) {
-
-	  // set camera to rotate around center of loaded object
-	  controls.target = center;
-
-	  // prevent camera from zooming out far enough to create far plane cutoff
-	  controls.maxDistance = cameraToFarEdge * 2;
-
-	  controls.saveState();
-
-	} else {
-
-		camera.lookAt( center )
-
-   }
-}
-
 // create the capture
 function saveAsImage() {
 	var imgData, imgNode;
