bc90fea760a67483986b596db7ecaeaf3bf6c60e
Ludovick <design@ludovick.ninja>
2022-01-30 11:44:39 -0500

    added AR2

---
+++ AR2/capture.js
@@ -0,0 +1,37 @@
+import { renderer, modelName } from './main.js';
+
+var strDownloadMime = "image/octet-stream";
+
+const captureControlButton = document.querySelector('.CaptureButton');
+captureControlButton.addEventListener("click", saveAsImage);
+
+// create the capture
+function saveAsImage() {
+	var imgData, imgNode;
+
+	try {
+		var strMime = "image/jpeg";
+		imgData = renderer.domElement.toDataURL(strMime);
+
+		saveFile(imgData.replace(strMime, strDownloadMime), modelName + ".jpg");
+
+	} catch (e) {
+		console.log(e);
+		return;
+	}
+
+}
+
+var saveFile = function (strData, filename) {
+	var link = document.createElement('a');
+	if (typeof link.download === 'string') {
+		document.body.appendChild(link); //Firefox requires the link to be in the body
+		link.download = filename;
+		link.href = strData;
+		link.click();
+		document.body.removeChild(link); //remove the link when done
+	} 
+	else {
+		location.replace(uri);
+	}
+}
