d4fb559747d3ff7a189a55b9736668a184369f6c
Ludovick <design@ludovick.ninja>
2022-01-30 14:16:06 -0500

    400px max dimensions to video

--- AR2/webrtc.js
+++ AR2/webrtc.js
@@ -98,7 +98,7 @@ function start() {
   const videoSource = videoSelect.value;
   const constraints = {
     //audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined, width: {max: 1024}, height: {max: 1024}, aspectRatio: {ideal: 1}}    
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined, width: {max: 400}, height: {max: 400}, aspectRatio: {ideal: 1}}    
   };
   navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
