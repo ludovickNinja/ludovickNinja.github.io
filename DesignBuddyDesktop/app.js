82afffebcd865f478208b195adfe4fa1d8e9cd5c
ludovickNinja <44409534+ludovickNinja@users.noreply.github.com>
2025-03-23 01:14:13 -0400

    Update app.js

--- DesignBuddyDesktop/app.js
+++ DesignBuddyDesktop/app.js
@@ -392,6 +392,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       id: "blog",
       title: "Blog",
+      hidden: true, // This marks it as hidden
       content: `
         <div id="blog-container">
           <div class="blog-controls">
